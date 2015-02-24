f57d28e3f6ad851dbfebc8b184420765daaceee5
Patrick O'Brien <obiedman@gmail.com>
2015-02-24 10:44:59 -0500

    backup

---
+++ js/video_categories.js
@@ -0,0 +1,286 @@
+var details = {
+  "look_alive" : {
+      "video" : "https://www.youtube.com/embed/42o1HCmFFpo",
+      "video_details" : {
+          "title" : "Look Alive",
+          "artist" : "Billy Dodge and the Emotions",
+          "blurb" : "Bllly Dodge, an exciting new Boston talent, performs 'Look Alive' at Johnny D's in Somerville, MA with his band, the Emotions. Filmed on the RED Scarlet and a Canon 60D."
+      }
+    },
+
+  "illusory" : {
+    "video" : "https://www.youtube.com/embed/2wdTTmL6CA4",
+    "video_details" : {
+      "title" : "Illusory",
+      "artist" : "Billy Dodge ft. Eleanor Kaufman",
+      "blurb" : "The official music video for 'Illusory' by Billy Dodge. Shot on the RED Scarlet in Westport, MA.)"
+    }
+  },
+  "nikes" : {
+    "video" : "https://www.youtube.com/embed/cZkMsOnjLPs?rel=0",
+    "video_details" : {
+      "title" : "Nikes",
+      "artist" : "Re-Up",
+      "blurb" : "The official music video for 'Nikes' by Re-Up. Filmed in Waltham, MA on the Sony EX1."
+    }
+  },
+  "yellow_woman" : {
+    "video" : "https://www.youtube.com/embed/uh4GYgv_XUU?rel=0",
+    "video_details" : {
+      "title" : "Yellow Woman",
+      "artist" : "The Nate Wilson Group",
+      "blurb" : "Nate Wilson group sings their hit song 'Yellow Woman' at Church in Boston, MA. Filmed on the Sony EX1."
+    }
+  },
+  "911" : {
+    "video" : "https://player.vimeo.com/video/48550551?color=4cff24&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "911 Emergency",
+      "artist" : "Sarah B ft. Scotch Bonnet",
+      "blurb" : "The official music video for '911 Emergency' by Sarah B, featuring Scotch Bonnet."
+    }
+  },
+  "jennah" : {
+    "video" : "https://player.vimeo.com/video/51732936?color=4cff24&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Jennah Bell LIVE",
+      "blurb" : "Oakland-grown singer/songwriter Jennah Bell singing live in studio. Edited by Bangarang Films. "
+    }
+  },
+  "high_hopes" : {
+    "video" : "https://player.vimeo.com/video/51732937?color=4cff24&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "High Hopes, Live @ Church",
+      "artist" : "Re-Up",
+      "blurb" : "Re-Up performing their hit song 'High Hopes' at Church in Boston, MA. Filmed on the Sony EX1."
+    }
+  },
+  "nothing" : {
+    "video" : "https://player.vimeo.com/video/51732396?color=4cff24&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Nothing Permanent, Live @ The MIddle East",
+      "artist" : "Re-Up",
+      "blurb" : "Re-Up performing their new song 'Nothing Permanent' at The Middle East in Cambridge, MA. Filmed on the Sony EX1."
+    }
+  },
+  "2way_camp" : {
+      "video" : "https://www.youtube.com/watch?v=NoFS1aVeF7g",
+      "video_details" : {
+          "title" : "2Way Lacrosse Camp",
+          "blurb" : "2Way Lacrosse is a youth program that specializes in world class lacrosse instruction. They seek to use the game of lacrosse to uplift and inspire young athletes to realize their full potential. 2Way offers many camp options in the New England and New York area."
+      }
+    },
+
+  "b4b" : {
+    "video" : "https://www.youtube.com/embed/yXmjtUPpv1I",
+    "video_details" : {
+      "title" : "Beggin' 4 Bass",
+      "blurb" : "Join host Adam Guild as he travels the country searching for the best bass fishing spots our nation has to offer. Expect plenty of food, fun, and general debauchery as 'AG' brings his unique sense of humor to the locals of these legendary fishing holes. Produced by Bangarang Films."
+    }
+  },
+  "2way_selects" : {
+    "video" : "https://www.youtube.com/embed/IFESGNUA090",
+    "video_details" : {
+      "title" : "2Way Select",
+      "blurb" : "DEVELOP as a player. COMPETE as a team. Have FUN. The goal at 2Way Select is to provide a unique and fun, first-class lacrosse experience for the dedicated and committed lacrosse player. Only through great teamwork do great individuals stand out."
+    }
+  },
+  "and1_live" : {
+    "video" : "https://player.vimeo.com/video/39435121?title=0&amp;byline=0&amp;portrait=0&amp;color=4cff24",
+    "video_details" : {
+      "title" : "And1 Live - Turks and Caicos",
+      "blurb" : "The AND1 Live team brings their high-flying dunks, head-spinning dribbling, and hilarious antics to Turks and Caicos in this highlight video of the trip."
+    }
+  },
+  "ecomagination" : {
+    "video" : "https://player.vimeo.com/video/51732943?color=4cff24&title=0&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Ecomagination",
+      "blurb" : "A commercial contest entry for a company's environmental initiative, filmed on the Canon 60D at Portsmouth Abbey School in Portsmouth, RI."
+    }
+  },
+  "vegas" : {
+    "video" : "https://www.youtube.com/embed/R3mfR-cm9Bc",
+    "video_details" : {
+      "title" : "More Money For You",
+      "blurb" : "A commercial contest entry for a casino's marketing campaign filmed in Las Vegas, NV. "
+    }
+  },
+  "apollo" : {
+    "video" : "https://www.youtube.com/embed/ubX5MfnTmWM",
+    "video_details" : {
+      "title" : "Apollo 543",
+      "blurb" : "A commercial contest entry for a sandwich chain's marketing campaign. It received an Editor's Choice award from Poptent."
+    }
+  },
+  "ripp_harris" : {
+    "video" : "https://www.youtube.com/embed/rd0ACHhLYbo",
+    "video_details" : {
+      "title" : "RIPP Charles Harris - Dead Men's Tales",
+      "blurb" : "A commercial produced for Dead Men's Tales, A Walking Tour of Newport's True Pirate Past.  Produced by JJ Rok and his classmates at CDIA."
+    }
+  },
+  "ripp_tew" : {
+    "video" : "https://www.youtube.com/embed/gJo4G1eH2lI",
+    "video_details" : {
+      "title" : "RIPP Thomas Tew - Dead Men's Tales",
+      "blurb" : "A commercial produced for Dead Men's Tales, A Walking Tour of Newport's True Pirate Past. Produced by JJ Rok and his classmates at CDIA."
+    }
+  },
+  "ripp_wall" : {
+    "video" : "https://www.youtube.com/embed/oYvxzyGXYMg",
+    "video_details" : {
+      "title" : "RIPP Rachel Wall - Dead Men's Tales",
+      "blurb" : "A commercial produced for Dead Men's Tales, A Walking Tour of Newport's True Pirate Past. Produced by JJ Rok and his classmates at CDIA."
+    }
+  },
+  "sniagrab" : {
+    "video" : "https://www.youtube.com/embed/jvxSOIvoXY0",
+    "video_details" : {
+      "title" : "SNIAGRAB",
+      "blurb" : "A commercial contest entry for a sporting goods chain's marketing campaign. It's a great example of what we can do for your brand using animation."
+    }
+  },
+  "qwest" : {
+    "video" : "https://www.youtube.com/embed/9IXbZMm8Xj8",
+    "video_details" : {
+      "title" : "QWEST",
+      "blurb" : "A commercial contest entry for a telecommunications carrier's marketing campaign. "
+    }
+  },
+  "bbb" : {
+    "video" : "https://www.youtube.com/embed/6j-YDRhIRZI",
+    "video_details" : {
+      "title" : "Code Blu: Bless By Bless & Ducati Pool Party",
+      "blurb" : "Models sport the Bless By Bless designs at the Bless by Bless & Ducati 128 Pool Party."
+    }
+  },
+  "cheesesteak" : {
+    "video" : "https://www.youtube.com/embed/5F9ifjd30Yg",
+    "video_details" : {
+      "title" : "The Cheesesteak Guys - T.C. Lando's",
+      "blurb" : "JJ Rok and his classmates at CDIA produced this web video for TC Lando's Subs and Pizzeria located in Acton, Clinton, Hudson, and Leominster, Massacusetts, and Carl's Steak Subs in Waltham, Massachusetts. The video highlights the owners' stories of the chain's history and the delicious product they offer."
+    }
+  },
+  "ecr" : {
+    "video" : "https://www.youtube.com/embed/4cVZKR2a0lg",
+    "video_details" : {
+      "title" : "East Coast Realty",
+      "blurb" : "East Coast Realty is one of Boston's premier real estate agencies. Their professionally trained salespeople are here to help you find an apartment that fits your needs at a time that’s convenient for you. Because they’re open every day of the week, an appointment is never necessary."
+    }
+  },
+  "horner_show" : {
+    "video" : "https://www.youtube.com/embed/sYKqq0eYQTo",
+    "video_details" : {
+      "title" : "Horner Millwork - Somerset Showroom",
+      "blurb" : "As one of the leading millwork companies in New England, Horner Millwork is a distributor and custom manufacturer of windows, doors, kitchens, and stairs to the residential and commercial markets."
+    }
+  },
+  "horner_prod" : {
+    "video" : "https://www.youtube.com/embed/f4wbR7nRK_U",
+    "video_details" : {
+      "title" : "Horner Millwork - Production Facility",
+      "blurb" : "Take a tour of the Horner Millwork Production Facility in Somerset, Massachusetts. As one of the leading millwork companies in New England, Horner is a distributor and custom manufacturer of windows, doors, kitchens, and stairs to the residential and commercial markets."
+    }
+  },
+  "pafr" : {
+    "video" : "https://www.youtube.com/embed/o76qOlDylvU",
+    "video_details" : {
+      "title" : "Pediatric Associates of Fall River",
+      "blurb" : "Pediatric Associates of Fall River is an independent general pediatric practice that has been serving the Greater Fall River area since 1974."
+    }
+  },
+  "westy" : {
+    "video" : "https://player.vimeo.com/video/112183637",
+    "video_details" : {
+      "title" : "An Aerial Tour of Westminster School",
+      "blurb" : "An aerial tour of Westminster School in Simsbury, Connecticut, shot during October of 2014."
+    }
+  },
+  "rmo" : {
+    "video" : "https://www.youtube.com/embed/uD26aFecAhc",
+    "video_details" : {
+      "title" : "Remodeler's Outlet",
+      "blurb" : "Remodeler's Outlet is a family owned home improvement retail store offering thousands of first quality products including kitchen and bath cabinetry, windows, interior doors, exterior entrance units, storm and patio doors, mouldings, decking, stair parts, hardware, and so much more."
+    }
+  },
+  "cookie" : {
+    "video" : "https://www.youtube.com/embed/U7j2oRn05-c",
+    "video_details" : {
+      "title" : "The Notorious C.O.O.K.I.E.",
+      "blurb" : "A music video parody created using Sesame Street clips of the Cookie Monster and Kermit the Frog, with a remix of the Notorious B.I.G. featuring Miley Cyrus. Over 150,000 views!"
+    }
+  },
+  "dougie" : {
+    "video" : "https://www.youtube.com/embed/a-Pj2aDQ6bs",
+    "video_details" : {
+      "title" : "Teach Me How to DOUGie",
+      "blurb" : "A music video parody created using clips from the show Doug and Cali Swag District's 'Teach Me How to Dougie'. Over 250,000 views!"
+    }
+  },
+  "geese" : {
+    "video" : "https://player.vimeo.com/video/44580427?color=4cff24&title=0&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Geese On the Charles River",
+      "blurb" : "First day testing our RED Scarlet on the Charles River in Watertown, MA. No color correction applied, this is all straight from the camera with a little tweaking of the metadata in post. "
+    }
+  },
+  "gringos" : {
+    "video" : "https://player.vimeo.com/video/51732942?color=4cff24&title=0&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Gringos Locos",
+      "blurb" : "A short film we threw together of our trip to Tulum, Mexico in March 2011. Experience the beaches, cenotes (underground caves), wildlife, and Mayan ruins that this unbelievable destination has to offer. It was a good trip to say the least."
+    }
+  },
+  "lion" : {
+    "video" : "https://www.youtube.com/embed/_5NZ5ZcMJCU",
+    "video_details" : {
+      "title" : "The Lion and the Unicorn",
+      "blurb" : "A short horror film loosely based on the old United Kingdom nursery rhyme, The Lion and the Unicorn. Shot and edited by JJ Rok."
+    }
+  },
+  "snow" : {
+    "video" : "https://player.vimeo.com/video/51732395?color=4cff24&title=0&byline=0&portrait=0",
+    "video_details" : {
+      "title" : "Snowed In",
+      "blurb" : "A camera test for the Canon 60D when we got stuck in New Hampshire while working on a script. "
+    }
+  },
+  "baking" : {
+    "video" : "https://player.vimeo.com/video/102031385",
+    "video_details" : {
+      "title" : "Baking Day at Grandma's",
+      "blurb": "A promotional trailer for 'Baking Day at Grandma's' a children's book by Anika Denise."
+    }
+  }
+}
+
+var videoDetailsHeight;
+
+var returnArtist = function(video){
+  if (video["video_details"]["artist"] == undefined){
+    return '';
+  } else{
+    return "<h3 class='text-secondary'>"+video["video_details"]["artist"] + "</h3>"
+  }
+};
+
+$(".video_link").click(function(){
+  $(".active").removeClass("active");
+  $(this).addClass("active");
+  var video = details[$(this).data("video")];
+  video_string = "<iframe class='embed-responsive-item padded' src=" + video["video"] + " allowfullscreen></iframe>"
+  details_string = "<div class='video_details' data-count='0'><h2 class='text-primary'>" + video["video_details"]["title"] + "</h2>" + returnArtist(video) + "<h3>" + video["video_details"]["blurb"] + "</h3></div>";
+  $("#video_window").html(video_string);
+  $("#details_window").html(details_string);
+
+});
+
+$(document).ready(function(){
+  // make heights equal
+  $(".video_list").height($(".video_info_pane").height());
+})
+
+$(window).resize(function(){
+  $(".video_list").height($(".video_info_pane").height());
+})
